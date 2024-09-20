b34ece315e43e5079d856bda8b915bdb2d1075fc
Mateus Freitas <mateus.freitas@e-unicred.com.br>
2024-09-20 17:44:54 -0300

    add topologias

---
+++ Readme.md
@@ -0,0 +1,68 @@
+## Redes de Computadores Avançadas - Prof. Tiago Coelho Ferreto
+
+# Trabalho 1 - simulador de roteamento multicast
+
+### Integrantes do grupo:
+
+- **Jeniffer Aparecida Klein Bittencourt**
+- **Mateus de Carvalho de Freitas**
+
+
+
+### Formato do arquivo de descrição de topologia
+
+#### SUBNET
+
+`<sid>,<netaddr/mask>`
+
+#### ROUTER
+
+`<rid>,<numifs>,<ip1/mask>,<weight1>,<ip2/mask>,<weight2>,<ip3/mask>,<weight3>...`
+
+#### MGROUP
+
+`<mid>,<sid1>,<sid2>,...,<sidn>`
+
+### Formato de saída
+
+#### Cabeçalho das tabelas de roteamento unicast: `#UROUTETABLE`
+
+**Entrada de roteamento unicast:**  
+`<rid>, <netaddr/mask>,<nexthop>,<ifnum>`
+
+#### Cabeçalho das tabelas de roteamento multicast: `#MROUTETABLE`
+
+**Entrada de roteamento multicast:**  
+`<rid>,<mid>,<nexthop1>,<ifnum1>,<nexthop2>,<ifnum2>,...,<nexthopn>,<ifnumn>`
+
+#### Cabeçalho da transmissão para o grupo multicast: `#TRACE`
+
+**Mensagem mping:**  
+`<sid|rid> =>> <sid|rid>, ..., <sid|rid> =>> <sid|rid>: mping <mgroupid>;`
+
+
+### Exexução partir de um terminal por linha de comando
+
+`python3 simulador.py topologia.txt s1 g1`
+
+Se estiver usando Linux e quiser executar sem digitar python3:
+
+#### 1. Dar permissão de execução ao arquivo:
+No terminal, execute:
+
+chmod +x simulador.py
+
+#### 2. Copiar o arquivo para `/usr/local/bin/`:
+Copie o arquivo para o diretório `/usr/local/bin/`, que geralmente está no `PATH`, usando o seguinte comando:
+
+sudo cp simulador.py /usr/local/bin/simulador
+
+#### 3. Executar o simulador:
+Agora, você poderá executar o simulador diretamente como um comando:
+
+simulador topologia.txt s1 g1
+
+
+
+
+
--- simulador.py
+++ simulador.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 import heapq
 from ipaddress import ip_network, ip_interface
@@ -9,10 +11,6 @@ multicast_groups = {}
 unicast_table = []
 mcast_table = []
 trace = []
-
-# Função auxiliar para extrair os 3 primeiros octetos de um IP
-def extract_prefix(ip):
-    return '.'.join(ip.split('.')[:3])
 
 class Router:
     def __init__(self, rid):
@@ -32,6 +30,191 @@ class Router:
 
     def __lt__(self, other):
         return self.rid < other.rid
+
+
+# Função auxiliar para extrair os 3 primeiros octetos de um IP
+def extract_prefix(ip):
+    return '.'.join(ip.split('.')[:3])
+
+# Função para verifica se o roteador tem uma conexão direta com a sub-rede
+def is_direta(router, subnet):
+    subnet_prefix = extract_prefix(ip_interface(subnet).ip.exploded)
+    # Itera sobre as interfaces para comparar o prefixo da sub-rede e capturar o iface_num
+    for iface_num, ip_mask, weight in router.interfaces:
+        if subnet_prefix == extract_prefix(ip_interface(ip_mask).ip.exploded):
+            return iface_num 
+    return False
+
+def dijkstra(start_rid, subnet):
+    distances = {rid: float('inf') for rid in routers}
+    distances[start_rid] = 0
+    previous_nodes = {}
+    
+    # Lista para armazenar caminhos com conexões diretas
+    direct_connections = []
+    
+    priority_queue = [(0, start_rid)]
+    while priority_queue:
+        current_distance, current_rid = heapq.heappop(priority_queue)
+
+        if current_distance > distances[current_rid]:
+            continue
+        
+        # Verifica se o roteador atual tem uma conexão direta com a sub-rede
+        is_direta_result = is_direta(routers[current_rid], subnet)
+        if is_direta_result is not False:
+            direct_connections.append((current_distance, current_rid))
+
+        for neighbor, (ip_mask_destino, iface_num_origem, weight) in routers[current_rid].neighbors.items():
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                previous_nodes[neighbor] = (current_rid, ip_mask_destino, iface_num_origem, distance)
+                heapq.heappush(priority_queue, (distance, neighbor))
+    
+    # Reconstrói o caminho de menor custo
+    path = None
+    
+    if direct_connections:
+        best_connection = min(direct_connections, key=lambda x: x[0])
+        _, best_rid = best_connection
+        
+        while best_rid != start_rid:
+            path = previous_nodes.get(best_rid)
+            best_rid = path[0]
+
+    return path
+
+def build_unicast_table(routers, subnets):
+    for rid, router in routers.items():
+        for _, subnet in subnets.items():
+            # verifica interfaces diretamente conectadas
+            iface_num = is_direta(router, subnet)
+            if iface_num is not False:
+                unicast_table.append((rid, subnet, '0.0.0.0', iface_num, 0))
+                continue
+            
+            _, iface_destino, iface_num_origem, distance = dijkstra(rid, subnet)
+            
+            unicast_table.append((rid, subnet, ip_interface(iface_destino).ip.exploded, iface_num_origem, distance))
+
+
+# Função para gerar a tabela multicast com base na tabela unicast
+def build_multicast_table(unicast_table, multicast_group, start_rid):
+    visited_routers = set()
+    visited_subnets = set()
+    
+    # Encontrar as subredes associadas ao grupo multicast
+    interested_subnets = multicast_groups[multicast_group]
+    
+    priority_queue = [(start_rid, 0)]
+    heapq.heapify(priority_queue)
+    
+    while priority_queue:
+        current_rid, cost = heapq.heappop(priority_queue)
+        
+        if current_rid in visited_routers:
+            continue  # Se o roteador já foi visitado, pular
+        
+        visited_routers.add(current_rid)
+        
+        nexthops = []
+        
+        # Itera sobre as sub-redes de interesse
+        for subnet_id in interested_subnets:
+            if subnet_id in visited_subnets:
+                continue  # Pular sub-rede já processada
+            
+            subnet = subnets[subnet_id]
+            # Procura entradas na tabela unicast para o roteador atual
+            for entry in unicast_table:
+                if entry[0] == current_rid and entry[1] == subnet:
+                    #verificar se tem alguem que acesa r3 com suto
+                    
+                    if entry[2] == '0.0.0.0':
+                        nexthops.append((entry[2], entry[3], (subnet_id, 0)))
+                        visited_subnets.add(subnet_id)
+                    else:
+                        # Verificar se já existe uma rota com custo menor para a subnet_id em mcast_table
+                        exist = False
+                        for e in mcast_table:
+                            if any(nh[0] == subnet_id and nh[1] <= (cost + entry[4]) for _, _, nh in e[2]):
+                                exist = True
+                                break
+                        
+                        if not exist:
+                            nexthops.append((entry[2], entry[3], (subnet_id, cost + entry[4])))
+                            
+                        next_router = next((rid for rid, router in routers.items() if router.belongs_to_router(entry[2])), None)
+                        heapq.heappush(priority_queue, (next_router, cost + entry[4]))
+        
+        if nexthops:
+            mcast_table.append((current_rid, multicast_group, nexthops))
+
+# Função para simular o envio da mensagem mping usando a tabela multicast
+def simulate_mping(start_subnet, start_rid, multicast_group, mcast_table):
+    trace.append(([(start_subnet, start_rid)], f"mping {multicast_group}"))
+    
+    current_rid = start_rid
+    
+    for entry in mcast_table:
+        nexthops = []
+        current_rid = entry[0]
+        for iface, _, (sid, _) in entry[2]:
+            if iface == '0.0.0.0':
+                nexthops.append((current_rid, sid))
+            else:
+                router_id = next((rid for rid, router in routers.items() if router.belongs_to_router(iface)), None)
+                nexthops.append((current_rid, router_id))
+        
+        trace.append((nexthops, f"mping {multicast_group}"))
+
+
+def simulate_routing(filename, start_subnet, multicast_group):
+    parse_topology(filename)
+
+    # Extrai o prefixo da sub-rede inicial (os primeiros 3 octetos do IP)
+    start_subnet_prefix = extract_prefix(ip_interface(subnets[start_subnet].network_address).exploded)
+
+    # Determina o roteador inicial com base na sub-rede
+    start_router = None
+    for rid, router in routers.items():
+        # Verifica se o roteador tem alguma interface na sub-rede inicial
+        if any(start_subnet_prefix == extract_prefix(ip_interface(ip_mask).ip.exploded) for _, ip_mask, _ in router.interfaces):
+            start_router = rid
+            break
+
+    if not start_router:
+        print(f"Erro: não foi encontrado um roteador com a subrede {start_subnet}")
+        return
+
+    # Gerando tabelas
+    build_unicast_table(routers, subnets)
+    
+    # Gerar tabela muticats
+    build_multicast_table(unicast_table, multicast_group, start_router)
+    
+    # Simular mensagem ping
+    simulate_mping(start_subnet, start_router, multicast_group, mcast_table)
+    
+    # Print the unicast routing table for each router
+    print("#UROUTETABLE")
+    for entry in unicast_table:
+        print(f"{entry[0]},{entry[1]},{entry[2]},{entry[3]}")
+        
+    # Exibe tabela de roteamento multicast
+    print("#MROUTETABLE")
+    for entry in mcast_table:
+        nexthops_str = ','.join(f"{hop[0]},{hop[1]}" for hop in entry[2])
+        print(f"{entry[0]},{entry[1]},{nexthops_str}")
+    
+    # Exibe o trace de mensagens mping
+    print("#TRACE")
+    for hops, message in trace:
+        hop_str = ', '.join(f"{src} => {dst}" for src, dst in hops)
+        print(f"{hop_str} : {message};")
+
 
 def parse_topology(filename):
     with open(filename, 'r') as file:
@@ -90,214 +273,13 @@ def parse_topology(filename):
                             r1.add_neighbor(r2.rid, ip_mask2, iface_num1, weight1)
                             r2.add_neighbor(r1.rid, ip_mask1, iface_num2, weight2)
 
-# Função para verifica se o roteador tem uma conexão direta com a sub-rede
-def is_direta(router, subnet):
-    subnet_prefix = extract_prefix(ip_interface(subnet).ip.exploded)
-    # Itera sobre as interfaces para comparar o prefixo da sub-rede e capturar o iface_num
-    for iface_num, ip_mask, weight in router.interfaces:
-        if subnet_prefix == extract_prefix(ip_interface(ip_mask).ip.exploded):
-            return iface_num
-        
-    return False
-    
-def dijkstra(start_rid, subnet):
-    distances = {rid: float('inf') for rid in routers}
-    distances[start_rid] = 0
-    previous_nodes = {}
-    
-    # Lista para armazenar caminhos com conexões diretas
-    direct_connections = []
-    
-    priority_queue = [(0, start_rid)]
-    while priority_queue:
-        current_distance, current_rid = heapq.heappop(priority_queue)
-
-        if current_distance > distances[current_rid]:
-            continue
-        
-        # Verifica se o roteador atual tem uma conexão direta com a sub-rede
-        is_direta_result = is_direta(routers[current_rid], subnet)
-        if is_direta_result is not False:
-            direct_connections.append((current_distance, current_rid))
-
-        for neighbor, (ip_mask_destino, iface_num_origem, weight) in routers[current_rid].neighbors.items():
-            distance = current_distance + weight
-
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                previous_nodes[neighbor] = (current_rid, ip_mask_destino, iface_num_origem, distance)
-                heapq.heappush(priority_queue, (distance, neighbor))
-    
-    # Reconstrói o caminho de menor custo
-    path = None
-    
-    if direct_connections:
-        best_connection = min(direct_connections, key=lambda x: x[0])
-        _, best_rid = best_connection
-        
-        while best_rid != start_rid:
-            path = previous_nodes.get(best_rid)
-            best_rid = path[0]
-
-    return path
-
-def build_unicast_table(routers, subnets):
-    for rid, router in routers.items():
-        for _, subnet in subnets.items():
-            # verifica interfaces diretamente conectadas
-            iface_num = is_direta(router, subnet)
-            if iface_num is not False:
-                unicast_table.append((rid, subnet, '0.0.0.0', iface_num, 0))
-                continue
-            
-            _, iface_destino, iface_num_origem, distance = dijkstra(rid, subnet)
-            
-            unicast_table.append((rid, subnet, ip_interface(iface_destino).ip.exploded, iface_num_origem, distance))
-
-# Função para gerar a tabela multicast com base na tabela unicast
-def build_multicast_table(unicast_table, multicast_group, start_rid):
-    visited_routers = set()
-    visited_subnets = set()
-    
-    # Encontrar as subredes associadas ao grupo multicast
-    interested_subnets = multicast_groups[multicast_group]
-    
-    priority_queue = [(start_rid, 0)]
-    heapq.heapify(priority_queue)
-    
-    while priority_queue:
-        current_rid, cost = heapq.heappop(priority_queue)
-        
-        if current_rid in visited_routers:
-            continue  # Se o roteador já foi visitado, pular
-        
-        visited_routers.add(current_rid)
-        
-        nexthops = []
-        
-        # Itera sobre as sub-redes de interesse
-        for subnet_id in interested_subnets:
-            if subnet_id in visited_subnets:
-                continue  # Pular sub-rede já processada
-            
-            subnet = subnets[subnet_id]
-            # Procura entradas na tabela unicast para o roteador atual
-            for entry in unicast_table:
-                if entry[0] == current_rid and entry[1] == subnet:
-                    #verificar se tem alguem que acesa r3 com suto
-                    
-                    if entry[2] == '0.0.0.0':
-                        nexthops.append((entry[2], entry[3], (subnet_id, 0)))
-                        visited_subnets.add(subnet_id)
-                    else:
-                        # Verificar se já existe uma rota com custo menor para a subnet_id em mcast_table
-                        exist = False
-                        for e in mcast_table:
-                            if any(nh[0] == subnet_id and nh[1] <= (cost + entry[4]) for _, _, nh in e[2]):
-                                exist = True
-                                break
-                        
-                        if not exist:
-                            nexthops.append((entry[2], entry[3], (subnet_id, cost + entry[4])))
-                            
-                        next_router = next((rid for rid, router in routers.items() if router.belongs_to_router(entry[2])), None)
-                        heapq.heappush(priority_queue, (next_router, cost + entry[4]))
-        
-        if nexthops:
-            mcast_table.append((current_rid, multicast_group, nexthops))
-
-# Função para simular o envio da mensagem mping usando a tabela multicast
-def simulate_mping(start_subnet, start_rid, multicast_group, mcast_table):
-    visited_routers = set()  # Para evitar loops e reenvios desnecessários
-    visited_subnets = set()  # Para controlar as sub-redes já atingidas
-
-    # Define as sub-redes interessadas
-    interested_subnets = multicast_groups[multicast_group]
-
-    trace.append(([(start_subnet, start_rid)], f"mping {multicast_group}"))
-    
-    current_rid = start_rid
-    visited_routers.add(current_rid)
-    
-    for sid in interested_subnets:
-        nexthops = []
-        
-        
-        for entry in mcast_table:
-            if entry[0] == current_rid and entry[1] == multicast_group:
-                for iface, _, _ in entry[2]:
-                    if iface == '0.0.0.0':
-                        nexthops.append((current_rid, sid))
-                        visited_subnets.add(sid)
-                        visited_routers.add(current_rid)
-                    else:
-                        router_id = next((rid for rid, router in routers.items() if router.belongs_to_router(iface)), None)
-                        nexthops.append((current_rid, router_id))
-        
-                current_rid = entry[0]
-                trace.append((nexthops, f"mping {multicast_group}"))
-
-def simulate_routing(filename, start_subnet, multicast_group):
-    parse_topology(filename)
-
-    # Extrai o prefixo da sub-rede inicial (os primeiros 3 octetos do IP)
-    start_subnet_prefix = extract_prefix(ip_interface(subnets[start_subnet].network_address).exploded)
-
-    # Determina o roteador inicial com base na sub-rede
-    start_router = None
-    for rid, router in routers.items():
-        # Verifica se o roteador tem alguma interface na sub-rede inicial
-        if any(start_subnet_prefix == extract_prefix(ip_interface(ip_mask).ip.exploded) for _, ip_mask, _ in router.interfaces):
-            start_router = rid
-            break
-
-    if not start_router:
-        print(f"Erro: não foi encontrado um roteador com a subrede {start_subnet}")
-        return
-
-    # Gerando tabelas
-    build_unicast_table(routers, subnets)
-    
-    # Gerar tabela muticats
-    build_multicast_table(unicast_table, multicast_group, start_router)
-    
-    # Simular mensagem ping
-    simulate_mping(start_subnet, start_router, multicast_group, mcast_table)
-    
-    # Print the unicast routing table for each router
-    print("#UROUTETABLE")
-    for entry in unicast_table:
-        print(f"{entry[0]},{entry[1]},{entry[2]},{entry[3]}")
-        
-    # Exibe tabela de roteamento multicast
-    print("#MROUTETABLE")
-    for entry in mcast_table:
-        nexthops_str = ','.join(f"{hop[0]},{hop[1]}" for hop in entry[2])
-        print(f"{entry[0]},{entry[1]},{nexthops_str}")
-    
-    # Exibe o trace de mensagens mping
-    print("#TRACE")
-    for hops, message in trace:
-        hop_str = ', '.join(f"{src} => {dst}" for src, dst in hops)
-        print(f"{hop_str} : {message};")
-
-
-
-
 if __name__ == "__main__":
-    # if len(sys.argv) != 4:
-    #     print("Uso: simulador <arquivo_topologia> <subnet_inicial> <multicast_group>")
-    #     sys.exit(1)
+    if len(sys.argv) != 4:
+        print("Uso: simulador <arquivo_topologia> <subnet_inicial> <multicast_group>")
+        sys.exit(1)
 
-    # topology_file = sys.argv[1]
-    # start_subnet = sys.argv[2]
-    # multicast_group = sys.argv[3]
-    topology_file = "topologia.txt"
-    start_subnet = "s1"
-    multicast_group = "g1"
+    topology_file = sys.argv[1]
+    start_subnet = sys.argv[2]
+    multicast_group = sys.argv[3]
 
     simulate_routing(topology_file, start_subnet, multicast_group)
-
-
-# <rid>,  <mid>,  <nexthop1>,<ifnum1>,
-#  r1,     g1,    0.0.0.0,1
